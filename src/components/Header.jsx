830038ae6e88c28df9c4955ba4d1c2f959576d6a
Connor North <153465136+CNDR96@users.noreply.github.com>
2025-06-24 12:23:04 +0100

    Start to flesh out file structure and update header

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <header>
+      <h1>NC News</h1>
+      <nav>
+        <ul>
+          <li>World News</li>
+          <li>Sports</li>
+          <li>Technology</li>
+          <li>Gaming</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
