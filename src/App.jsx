e38356334bf3ebc41050a3403f3415b5f87491b5
Connor North <153465136+CNDR96@users.noreply.github.com>
2025-06-24 15:05:13 +0100

    Set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
-import Header from "./components/Header";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ArticlePage from "./pages/ArticlePage";
+import TopicPage from "./pages/TopicPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
     <>
-      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/articles/:article_id" element={<ArticlePage />} />
+        <Route path="/topics/:topic_slug" element={<TopicPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
   );
 }
---
+++ src/pages/ArticlePage.jsx
@@ -0,0 +1,3 @@
+function ArticlePage() {}
+
+export default ArticlePage;
