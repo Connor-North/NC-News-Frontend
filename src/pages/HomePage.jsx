b485d6fe1f277235ff77f8aee698991d1adaab05
Connor North <153465136+CNDR96@users.noreply.github.com>
2025-06-25 09:04:11 +0100

    Implement initial HomePage view

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,42 @@
-function HomePage() {}
+import { useDebugValue, useEffect, useState } from "react";
+import ArticleCard from "../components/ArticleCard";
+
+function HomePage() {
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://nc-news-v7di.onrender.com/api/articles/")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to fetch articles");
+        }
+        return res.json();
+      })
+      .then((res) => {
+        setArticles(res.articles);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <p>Loading articles...</p>;
+  if (error) return <p>{error}</p>;
+
+  return (
+    <main>
+      <h1>Articles</h1>
+      <ul>
+        {articles.map((article) => {
+          <ArticleCard key={article.article_id} article={article} />;
+        })}
+      </ul>
+    </main>
+  );
+}
 
 export default HomePage;
