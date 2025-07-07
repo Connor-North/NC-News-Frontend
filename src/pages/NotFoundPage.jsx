d659ed785463582eacd7871dbc84fc92eccb8d81
Connor North <153465136+CNDR96@users.noreply.github.com>
2025-07-07 15:16:04 +0100

    Add notfound page and update gitignore

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,3 +1,10 @@
-function NotFoundPage() {}
+function NotFoundPage() {
+  return (
+    <main>
+      <h1>404 - Page Not Found</h1>
+      <p>Oops! The page you're looking for doesn't exist.</p>
+    </main>
+  );
+}
 
 export default NotFoundPage;
